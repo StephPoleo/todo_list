029b9afadb2a0d86ac6077ef636c0ff8c6afa93b
Stephanie <stephaniepoleo@hotmail.com>
2022-09-26 15:34:25 -0500

    Defining a model for todo and adding it to admin site

--- todoProject/todoApp/models.py
+++ todoProject/todoApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class TodoModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    is_complete = models.BooleanField(null=True, blank=True)
+    parent = models.ForeignKey(
+        'TodoModel',
+        related_name='children',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        db_table = "todo"
+
+    def __str__(self):
+        return "{} - (Is Complete: {}) [{}]".format(self.name, self.is_complete, self.id)
